bd317112ff64b5493cc9e7479b14b36784f0f6b0
허정근 <gjek136@naver.com>
2018-11-27 02:51:58 +0900

    KaKaoTalk 예시로noti 세분화방법

--- app/src/main/java/com/proj3ct/perfectstranger/Notification/NotificationService.java
+++ app/src/main/java/com/proj3ct/perfectstranger/Notification/NotificationService.java
@@ -20,6 +20,12 @@ public class NotificationService extends NotificationListenerService {
     Context context;
     private boolean On = true;
 
+    private static final String KAKAO = "com.kakao.talk";
+    private static final String FACEBOOK = "com.facebook.orca";
+    private static final String FACEBOOK_REPLY = "com.facebook.katana";
+    private static final String CALL_IN = "com.samsaung.android.incallui"; // 내용1 : 현재통화 ,내용2 : 수신전화
+    private static final String MESSAGE = "com.facebook.orca";
+    private static final String INSTAGRAM = "com.instagram.android";
 
     @Override
     public void onCreate( ) {
@@ -42,6 +48,7 @@ public class NotificationService extends NotificationListenerService {
         }
         if (sbn == null) return;
         String packName = sbn.getPackageName();
+
         Notification mNotification = sbn.getNotification();
         String mainTitle;
         String subText;
@@ -49,57 +56,54 @@ public class NotificationService extends NotificationListenerService {
 
         String text = "";
         Bundle extras = mNotification.extras;
+        Intent intent;
+        intent = getNotiIntext(sbn);
+
 
         extras = mNotification.extras;
         if(extras.getString(Notification.EXTRA_TITLE) != null) {
             mainTitle = extras.getString(Notification.EXTRA_TITLE);
             subText = extras.getString(Notification.EXTRA_SUB_TEXT);
-            mainText =extras.getString(Notification.EXTRA_TEXT);
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_INFO_TEXT));
-//            Log.d("BACKGROUND_IMAGE_URI",extras.getString(Notification.EXTRA_BACKGROUND_IMAGE_URI));
-//            Log.d("EXTRA_TEXT_LINES",extras.getString(Notification.EXTRA_TEXT_LINES));
-            Log.d("EXTRA_TEXT",extras.getString(Notification.EXTRA_TEXT));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_BIG_TEXT));
-//            Log.d("EXTRA_SUB_TEXT",extras.getString(Notification.EXTRA_SUB_TEXT));
-            Log.d("EXTRA_TITLE",extras.getString(Notification.EXTRA_TITLE));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_AUDIO_CONTENTS_URI));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_CHANNEL_ID));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_CHRONOMETER_COUNT_DOWN));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_COLORIZED));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_COMPACT_ACTIONS));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_CONVERSATION_TITLE));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_HISTORIC_MESSAGES));
-//            Log.d("EXTRA_LARGE_ICON_BIG",extras.getString(Notification.EXTRA_LARGE_ICON_BIG));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_MEDIA_SESSION));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_MESSAGES));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_NOTIFICATION_ID));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_NOTIFICATION_TAG));
-//            Log.d("EXTRA_PEOPLE",extras.getString(Notification.EXTRA_PEOPLE));
-//            Log.d("EXTRA_PICTURE",extras.getString(Notification.EXTRA_PICTURE));
-//            Log.d("EXTRA_PROGRESS",extras.getString(Notification.EXTRA_PROGRESS));
-//            Log.d("PROGRESS_INDETERMINATE",extras.getString(Notification.EXTRA_PROGRESS_INDETERMINATE));
-//            Log.d("EXTRA_PROGRESS_MAX",extras.getString(Notification.EXTRA_PROGRESS_MAX));
-//            Log.d("EXTRA_SHOW_CHRONOMETER",extras.getString(Notification.EXTRA_SHOW_CHRONOMETER));
-//            Log.d("EXTRA_SHOW_WHEN",extras.getString(Notification.EXTRA_SHOW_WHEN));
-//            Log.d("EXTRA_SUMMARY_TEXT",extras.getString(Notification.EXTRA_SUMMARY_TEXT));
-//            Log.d("INFO_TEXT",extras.getString(Notification.EXTRA_TEMPLATE));
-            Log.d("EXTRA_TITLE_BIG",extras.getString(Notification.EXTRA_TITLE_BIG));
-            Intent intent = new Intent("Msg");
+            mainText = extras.getString(Notification.EXTRA_TEXT);
+
+
             intent.putExtra("mainTitle", mainTitle);
             intent.putExtra("subText", subText);
             intent.putExtra("mainText", mainText);
             intent.putExtra("appName", packName);
             LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
         }
+    }
 
-       /* if (text != null) {
-            Intent intent = new Intent("Msg");
-            intent.putExtra("text", text);
-            LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
-        }*/
-//
-//        String dateS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA).format(mNotification.when);
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private Intent getNotiIntext(StatusBarNotification sbn) {
+        Intent intent = new Intent("Msg");
+        String appName = sbn.getPackageName();
+        String mainTitle = "";
+        String subTitle = "";
+        String mainText = "";
+        Notification mNotification = sbn.getNotification();
+        Bundle extras = mNotification.extras;
+        if(extras.getString(Notification.EXTRA_TITLE) != null) {
+            return null;
+        }
+        // kakaoTalk
+        if(appName.equals(KAKAO)){
+            // 단체톡
+            if(extras.getString(Notification.EXTRA_TEXT) != null){
+                mainTitle = extras.getString(Notification.EXTRA_TEXT);
+                subTitle = extras.getString(Notification.EXTRA_TITLE);
+                mainText = extras.getString(Notification.EXTRA_TEXT);
+            }
+            // 개인톡
+            else{
+                mainTitle = extras.getString(Notification.EXTRA_TITLE);
+                mainText = extras.getString(Notification.EXTRA_TEXT);
+            }
+        }
 
+
+        return intent;
     }
 
     @Override
@@ -130,84 +134,3 @@ public class NotificationService extends NotificationListenerService {
 }
 
 
-//    // StatusBarNotification 내용 가공
-//    // https://www.learn2crack.com/2014/11/reading-notification-using-notificationlistenerservice.html
-//    // http://highway-to-j.blogspot.kr/2015/11/no3-notification-listener-caturing.html
-//    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-//    private String getNotiText(StatusBarNotification sbn) {
-//        String packName = sbn.getPackageName();
-//        Notification mNotification = sbn.getNotification();
-//        Bundle extras = mNotification.extras;
-////        text += "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" + "\n";
-////        text += "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" + "\n";
-////        text += "========notification 정보=========" + "\n";
-////        text += "#StatusBarNotification sbn의 정보" + "\n";
-////        text += "#sbn.getGroupKey() : + " + sbn.getGroupKey() + "\n";
-////        text += "#sbn.getKey() : + " + sbn.getKey() + "\n";
-////        text += "sbn.getOverrideGroupKey()+ " + sbn.getOverrideGroupKey() + "\n";* 요거는 24api 이상
-////        text += "#sbn.getPackageName() : + " + sbn.getPackageName() + "\n";
-////        text += "#sbn.getTag() : + " + sbn.getTag() + "\n";
-////        text += "#sbn.getId() : + " + Integer.toString(sbn.getId()) + "\n";
-////        text += "sbn.getGroupKey()+ " + sbn.getNotification() + "\n"; 이거는 객체
-////        text += "#sbn.getPostTime() : + " + Long.toString(sbn.getPostTime())+ "\n";
-////        text += "\n";
-////        text += "========sbn.getNotification의정보========" + "\n";
-////
-////        text += "mNotification.get" + mNotification.getChannelId() + "\n"; 26이상
-////        text += "#mNotification.getGroup : " + mNotification.getGroup() + "\n";
-////        text += "mNotification.get" + mNotification.getShortcutId() + "\n"; 26이상
-////        text += "#mNotification.getSortKey : " + mNotification.getSortKey() + "\n";
-////        text += "mNotification.get" + mNotification.getBadgeIconType() + "\n"; 26이상
-////        text += "mNotification.get" + mNotification.getGroupAlertBehavior() + "\n"; 26이상
-////        text += "#mNotification.get : " + /*mNotification.getLargeIcon()*/ " icon 형식임 ;" + "\n";
-////        text += "#mNotification.get : " + /*mNotification.getSmallIcon()*/  " icon 형식임 ;" + "\n" + "\n";
-////        text += "mNotification.get" + mNotification.getTimeoutAfter() + "\n"; 26이상
-//        extras = mNotification.extras;
-//        String mainTitle = "";
-//        String subTitle = "";
-//        String mainText = "";
-//        String appName = packName;
-//        String text;
-//        if(extras.getString(Notification.EXTRA_TITLE) != null) {
-//            mainTitle = extras.getString(Notification.EXTRA_TITLE);
-//            subTitle = extras.getString(Notification.EXTRA_TITLE);
-//            mainText =extras.getString(Notification.EXTRA_TEXT);
-//            firebaseDB.sendMessage(mainTitle,subTitle,mainText,appName);
-//
-//        }
-//        extras = mNotification.extras;
-//
-//        String dateS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA).format(mNotification.when);
-//
-///*        if (packName.equalsIgnoreCase("com.whatsapp")) {
-//            title = extras.getString(Notification.EXTRA_TITLE);
-//            content = extras.getString(Notification.EXTRA_TEXT);
-//        } else if (packName.equalsIgnoreCase("com.facebook.orca")){
-//            title = extras.getString(Notification.EXTRA_TITLE);
-//            content = extras.getString(Notification.EXTRA_TEXT);
-//        }
-//        else if (packName.equalsIgnoreCase("com.kakao.talk")) {
-//            text = mNotification.
-//        }
-//        else {
-//            if(mNotification.tickerText != null) {
-//                if(tmp != "null") {
-//                    tmp = mNotification.tickerText.toString();
-//                    text = "==== " + dateS + "\n(from) " + tmp;
-//                    tmp = extras.getString("android.title");
-//                    text += "\n(Title) " + tmp;
-//                    tmp = extras.getCharSequence("android.text").toString();
-//                    text += "\n(Text) " + tmp;
-//                }
-//            }
-//        }
-//
-//        if(tmp != "null"){
-//            text = "==== "+dateS+"\nkakao>> (Title) " + tmp;
-//            tmp = extras.getString(Notification.EXTRA_TEXT);
-//            text += "\n(Text) " + tmp;
-//        }*/
-//
-//
-//        return null;
-//    }
