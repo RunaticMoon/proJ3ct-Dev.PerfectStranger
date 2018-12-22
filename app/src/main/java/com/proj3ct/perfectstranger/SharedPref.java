f913e28e214ca53bcba4756b157dd121a609d712
RunaticMoon <39324508+RunaticMoon@users.noreply.github.com>
2018-12-23 08:33:05 +0900

    코드정리 2

---
+++ app/src/main/java/com/proj3ct/perfectstranger/SharedPref.java
@@ -0,0 +1,54 @@
+package com.proj3ct.perfectstranger;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class SharedPref {
+    private Context context;
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public String getRoomKey() {
+        SharedPreferences pref = context.getSharedPreferences("room", Context.MODE_PRIVATE);
+        return pref.getString("roomKey", null);
+    }
+
+    public String getUserKey() {
+        SharedPreferences pref = context.getSharedPreferences("room", Context.MODE_PRIVATE);
+        return pref.getString("userKey", null);
+    }
+
+    public User getUser() {
+        SharedPreferences pref = context.getSharedPreferences("room", Context.MODE_PRIVATE);
+        String strUser = pref.getString("user", null);
+        if(strUser != null) {
+            Gson gson = new GsonBuilder().create();
+            return gson.fromJson(strUser, User.class);
+        }
+        else
+            return null;
+    }
+    public void setPref(String roomKey, String userKey, User user) {
+        SharedPreferences pref = context.getSharedPreferences("room", Context.MODE_PRIVATE);
+
+        Gson gson = new GsonBuilder().create();
+        String strUser = gson.toJson(user, User.class);
+
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("roomKey", roomKey);
+        editor.putString("userKey", userKey);
+        editor.putString("user", strUser);
+        editor.apply();
+    }
+    public void destroy() {
+        SharedPreferences pref = context.getSharedPreferences("room", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.apply();
+    }
+}
